665a4e7fb37ea2a76d995460d3ccdfe9d9f9199c
Ashwin Jain <ashwinj984@gmail.com>
2025-04-05 02:32:30 +0530

    Minor change

--- app/Components/Custom/CheckList/CheckList.tsx
+++ app/Components/Custom/CheckList/CheckList.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Checkbox, Text } from "../../core";
+import { Box, Text } from "../../core";
 import { CustomCheckbox } from "../CustomCheckbox";
 import { theme } from "@/app/src";
 
